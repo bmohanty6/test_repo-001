1bdf3e439d4bb6e153566d6b32964f299817b63a
Biswakalyana Mohanty <bmohanty6@csc.com>
2013-12-13 16:19:43 +0530

    changed redirect.php

--- redirect.php
+++ redirect.php
@@ -3,4 +3,9 @@ print_r($_GET);
 print_r($_REQUEST);
 print_r( HttpRequest::getHeaders ());
 print_r( HttpRequest::getQueryData ());
+
+print_r($_GET);
+print_r($_REQUEST);
+print_r( HttpRequest::getHeaders ());
+print_r( HttpRequest::getQueryData ());
 ?>
